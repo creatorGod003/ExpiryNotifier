8732cd31267198cc382887c9414b0440975cf4d1
Ashutosh Ranjan <ranjan.ashutosh2003@gmail.com>
2022-11-21 18:29:50 +0530

    modularize the logic - consumed remains

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-This website tracks expiry dates of all the items that you have in your place. Just you have to manually entered the details of your item first time with expiry date and it will notify deadline for you.
+This website tracks expiry dates of all the items that you have in your place. Just you have to manually entered the details of your item first time with expiry date and it will track expiry dates of items for you.
 
 Technology involved: 
-* Javascript (local storage, dom manipulation, 
+* Javascript (local storage, dom manipulation)
 * Html
+* Css 
 * Scss
 
 Features: 
--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-
 updateTableContent();
-var count = 0;
+
 document.getElementById("add").addEventListener("click", function () {
+
   var item = document.getElementById("item-name").value;
   var qty = document.getElementById("qty-no").value;
   var date = document.getElementById("doe").value;
 
-  // making object of above data and store it in array present in local storage
+  if(!validate(date, qty)||item==""){
+    alert(`
+    Check if you have added passed date or today's date.
+    Check if quantity if negative or zero.
+    Check if any of the fields is empty.
+    `);
+  }
+  else {
+  
   const arr = JSON.parse(localStorage.getItem("data-items"));
 
-  // adding color to items whose month is same as registered item month
-  var color="" ;
-  
-      var itemMonth = new Date(date).getMonth();
-      var currentMonth = new Date().getMonth();
-  
-      if( itemMonth === currentMonth ){
-        color  = "red";
-      }
+  var color = "";
+
+  var itemMonth = new Date(date).getMonth();
+  var currentMonth = new Date().getMonth();
+
+  if (itemMonth === currentMonth) {
+    color = "red";
+  }
 
   const item_obj = {
     item: item,
@@ -29,27 +36,10 @@ document.getElementById("add").addEventListener("click", function () {
 
   localStorage.setItem("data-items", JSON.stringify(arr));
 
-  if (validate(date, qty)) {
-    document.getElementById("table-content").innerHTML += `<tr class="row ${color}">
-<td>${item}</td>
-<td class="middle-col">${qty}</td>
-<td>${date}</td>
-<td class="rightmost-col"><input type="checkbox" id="${count}"></td>
-</tr>`;
-    count++;
-    document.getElementById("item-name").value = "";
-    document.getElementById("qty-no").value = "";
-    document.getElementById("doe").value = "";
-  } else {
-    alert(`
-    Check if you have added passed date or today's date.
-    Check if quantity if negative or zero.
-    Check if any of the fields is empty.
-    `);
+  window.location.reload();
+  
   }
-
-  addCheckBoxListener();
-
+  
 });
 
 function validate(date, qty) {
@@ -71,112 +61,86 @@ function validate(date, qty) {
   return b;
 }
 
-function updateTableContent(){
-  if (localStorage.getItem("data-items") == null) {
-    const data_array = [];
-    localStorage.setItem("data-items", JSON.stringify(data_array));
-  } else {
-    const arr = JSON.parse(localStorage.getItem("data-items"));
-  
-    // comparator function
-    arr.sort(function (obj1, obj2) {
-      const d1 = new Date(obj1.date);
-      const d2 = new Date(obj2.date);
-  
-      if (d1 > d2) {
-        return 1;
-      } else if (d1 == d2) {
-        return 0;
-      } else {
-        return -1;
-      }
-    });
-  
-    for (var i = 0; i < arr.length; i++) {
-  
-      let color="" ;
-  
-      let itemMonth = new Date(arr[i].date).getMonth();
-      let currentMonth = new Date().getMonth();
-  
-      if( itemMonth === currentMonth ){
-        color  = "red";
-      }
-      
-      document.getElementById("table-content").innerHTML += `<tr class="row ${color} ">
-          <td>${arr[i].item}</td>
-          <td class="middle-col ">${arr[i].qty}</td>
-          <td>${arr[i].date}</td>
-          <td class="rightmost-col" ><input type="checkbox" ></td>
-          </tr>`;
-    }
-  
+function updateTableContent() {
+
+  document.getElementById("table-content").innerHTML = "";
+
+  if (localStorage.getItem("data-items") == null) { // if the data-items array is not present in local storage creating local storage
+      const data_array = [];
+      localStorage.setItem("data-items", JSON.stringify(data_array));
   }
+  else { 
+
+    const arr = JSON.parse(localStorage.getItem("data-items"));
+    
+    arr.sort(dateComparator); // sort the data according to date in ascending order.
+
+    localStorage.setItem("data-items", JSON.stringify(arr));
+
+    highlightRows();
+
+  }
+      
 }
 
 
+function dateComparator(obj1, obj2) {
+  const d1 = new Date(obj1.date);
+  const d2 = new Date(obj2.date);
 
+  if (d1 > d2) {
+    return 1;
+  } else if (d1 == d2) {
+    return 0;
+  } else {
+    return -1;
+  }
+}
 
+function highlightRows(){
 
-function refresh(){
+  const arr = JSON.parse(localStorage.getItem('data-items'));
+  arr.forEach(colorIt);
 
-  const arr = JSON.parse(localStorage.getItem("data-items"));
+}
 
-  document.getElementById("table-content").innerHTML=""
+function colorIt(val,i, arr){
 
-  for (var i = 0; i < arr.length; i++) {
-  
-    let color="" ;
+  console.log(val);
 
-    let itemMonth = new Date(arr[i].date).getMonth();
+    let color = "";
+    let itemMonth = new Date(val.date).getMonth();
     let currentMonth = new Date().getMonth();
 
-    if( itemMonth === currentMonth ){
-      color  = "red";
+    if (itemMonth === currentMonth) {
+      color = "red";
+    }
+    if(new Date(val.date)>=new Date()){
+
+      document.getElementById("table-content").innerHTML += `<tr class="row" style="background-color:${color};">
+      <td>${val.item}</td>
+      <td class="middle-col ">${val.qty}</td>
+      <td>${val.date}</td>
+      <td class="rightmost-col" ><input type="checkbox" id=${"val"+i} ></td>
+      </tr>`;
+
+    }
+    else{
+      arr.splice(i,1);
     }
     
-    document.getElementById("table-content").innerHTML += `<tr class="row ${color} ">
-        <td>${arr[i].item}</td>
-        <td class="middle-col ">${arr[i].qty}</td>
-        <td>${arr[i].date}</td>
-        <td class="rightmost-col" ><input type="checkbox" ></td>
-        </tr>`;
-  }
 
+    console.log(i);
 }
 
-addCheckBoxListener();
 
-function addCheckBoxListener(){
-  for(var x of document.querySelectorAll('#table-content  input[type="checkbox"]')){
+function clearStorage(){
 
-    console.log(x);
-  
-    x.addEventListener('click',function(){
-      let updatedQty = prompt('how many items you have consumed? ');
-  
-      let data_array = JSON.parse(localStorage.getItem('data-items'));
-  
-      let actualQty = data_array[Number(x.id)].qty;
-  
-      if(actualQty-updatedQty>=0){
-        data_array[Number(x.id)].qty -=updatedQty;
-        
-        if(actualQty-updatedQty==0)
-        data_array.splice(Number(x.id), 1);
-        
-      localStorage.setItem('data-items', JSON.stringify(data_array));
-  
-     
-      refresh();
-  
-      }
-      else{
-        alert('entered quantity is not valid');
-      }
-  
-  })
-  
+  var x = confirm("Are you sure that you want to delete all data?")
+
+  if(x==true){
+    localStorage.clear();
+    window.location.reload();
   }
-  
+
 }
